29006154c6d1a9da87ba68583fcb21b25f3298ab
MaX2708 <grin.maxim.2708@yandex.ru>
2022-06-10 17:32:33 +0400

    casino page text

--- pages/casinos/[casino].js
+++ pages/casinos/[casino].js
@@ -112,6 +112,24 @@ export default function CasinoPage({ casino }) {
                         dataText="2018"
                     />
                 </div>
+                <div className={styles.casinoText}>
+                    <div className={styles.casinoTextBlock}>
+                        <span className={styles.blockTitle}>
+                            About casino
+                        </span>
+                        <span>
+                            Parimatch offers 22 legal sport betting, cybersport and entertainment. As for covered countries, football coverage is decent, with match odds from 34 countries around the world, except international competitions. English game moves to the Premier-League Rayman - 7th level Even bookmaker’s office Parimatch is well known to the players, who are not particularly keen on counting sporting events, this kind of casino brand is not popular. But the advantages of this casino make you believe that the visiting will increase. Casino Parimatch offers excellent games, multilingual interface and qualified support team, all the most convenient payment methods and etc.
+                        </span>
+                    </div>
+                    <div className={styles.casinoTextBlock}>
+                        <span className={styles.blockTitle}>
+                            Verdict
+                        </span>
+                        <span>
+                            We have thoroughly checked Casino Parimatch and assessed a good reputation. It’s a great casino, yet there are some things to be paid attention to. In our review we looked at the casino players’ complaints, estimated revenue, license, the authenticity of the games, quality of client support, fairness of the condition, limits on withdrawals, winnings and other factors. Read the full review below and find out more about this casino. According to our research and estimates, casino Parimatch is a middle-sized online casino in terms of revenue. Income of the casino is an important factor. Large casinos shouldn't have problems paying out big wins, as such occurrences are more likely to happen big winnings in small casinos. We have found 1 complaint directly to this casino. Because of this complaint we gave this casino 576 dark points. You can find more information about these complaints and dark points below. Casino Parimatch - is a good casino. We regarded this rating because it manages many things. But there are some facts which deprive us to estimate it as a good or perfect reputation. Despite this, you can expect this casino to be a good place to play.
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
