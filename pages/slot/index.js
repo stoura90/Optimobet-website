1efa9a07aa4d6f288bfdc6568bd6a01e202df7e7
MaX2708 <grin.maxim.2708@yandex.ru>
2022-06-16 17:24:55 +0400

    slots page

--- pages/slot/index.js
+++ pages/slot/index.js
@@ -1,7 +1,90 @@
 import styles from '../../styles/pages/SlotPage.module.css'
-import Image from 'next/image';
+import Image from 'next/image'
+import useWindowSize from '../../hooks/useWindowSize'
+import Slot from '../../components/Slot'
+import { useState, useEffect } from 'react'
+import { SwiperSlide } from 'swiper/react';
+import SliderWithControls from '../../components/SliderWithControls'
+
+const _slots = [
+    {
+        id: 1,
+        name: 'Slot 1',
+        provider: 'ELK Studio',
+        type: 'Achievement',
+        rating: 4.5,
+    },
+    {
+        id: 2,
+        name: 'Slot 2',
+        provider: 'Netend',
+        type: 'Megaways',
+        rating: 4.5,
+    },
+    {
+        id: 3,
+        name: 'Slot 3',
+        provider: 'YGGDRASIL',
+        type: 'Bonus Buy',
+        rating: 4.5,
+    },
+    {
+        id: 4,
+        name: 'Slot 4',
+        provider: 'EGT',
+        type: 'Sticky Features',
+        rating: 4.5,
+    },
+    {
+        id: 5,
+        name: 'Slot 5',
+        provider: 'PRAGMATICPLAY',
+        type: 'Jackpot',
+        rating: 4.5,
+    },
+    {
+        id: 6,
+        name: 'Slot 6',
+        provider: 'Booming Games',
+        type: 'Achievement',
+        rating: 4.5,
+    },
+    {
+        id: 7,
+        name: 'Slot 7',
+        provider: 'IRON DOG',
+        type: 'Megaways',
+        rating: 4.5,
+    },
+    {
+        id: 8,
+        name: 'Slot 8',
+        provider: 'BETSOFT',
+        type: 'Bonus Buy',
+        rating: 4.5,
+    },
+    {
+        id: 9,
+        name: 'Slot 9',
+        provider: '2by2gaming',
+        type: 'Sticky Features',
+        rating: 4.5,
+    }
+]
 
 export default function SlotPage() {
+    const { width, height } = useWindowSize()
+	const [ offsetSlots, setOffsetSlots ] = useState()
+
+	useEffect(()=>{
+		const chunkSize = Math.trunc(width*0.8/(250+19))
+		let offset = []
+		for (let i = 0; i < _slots.length; i += chunkSize) {
+			offset.push(_slots.slice(i, i + chunkSize))
+		}
+		setOffsetSlots(offset)
+	},[width])
+
     return (
         <div className={styles.container}>
             <div className={styles.slotBlock}>
@@ -117,8 +200,69 @@ export default function SlotPage() {
                     </div>
                 </div>
             </div>
+            <div className={styles.slotPage}>
+                <div className={styles.emptyBlocks}>
+                    <div />
+                    <div />
+                </div>
+                <div className={styles.aboutSlot}>
+                    <span>
+                        Взломайте химический набор, наденьте пару защитных очков и приготовьтесь экспериментировать с древним искусством алхимии с этим игровым автоматом от Euro Games Technology. Знали ли вы, что древние алхимики стремились превратить все, что они могли получить в руки, в золото? Что ж, вы можете изучить Секреты алхимии и, возможно, получить в свои руки свою долю золота с этим игровым автоматом с 5 барабанами и 25 линиями выплат. С помощью болгарских разработчиков программного обеспечения Euro Games Technology в качестве катализатора этого реактивного игрового автомата вы можете быть уверены, вы получите достойный результат от качественного развлечения от Secrets of Alchemy.
+                    </span>
+                    <div className={styles.ourScore}>
+                        <div className={styles.ourScoreHeader}>
+                            <span className={styles.scoreHeaderText}>
+                                Our Score
+                            </span>
+                            <span className={styles.scorePoints}>
+                                7.2
+                            </span>
+                        </div>
+                        <span className={styles.ourScoreText}>
+                            Интересная и несложная игра. Тем не менее, в графике нет ничего особенного, особенно по сравнению с другими игровыми автоматами, доступными в настоящее время.
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.slotsSlider}>
+                <div className={styles.slotsSliderHeader}>
+                    <div className={styles.slotsSliderTitle}>
+                        <span className={styles.secondText}>
+                            Secondary
+                        </span>
+                        <span className={styles.mainText}>
+                            Main
+                        </span>
+                    </div>
+                    <div className={styles.seeMore}>
+                        See More
+                    </div>
+                </div>
+                {offsetSlots && offsetSlots.length>0 &&
+					<SliderWithControls>
+						{offsetSlots.map((item, index) => (
+							<SwiperSlide className={styles.slotSlide} key={index}>
+								{item.map(slot => (
+									<div 
+										style={{
+											width:"calc((100% - "+30*(Math.trunc(width*0.8/(250+19)) - 1)+"px)/"+Math.trunc(width*0.8/(250+19))+")", 
+											flex:"initial"
+										}}
+										key={`slot_${slot.id}`}
+									>
+										<Slot
+            							    {...slot}
+            							/>
+									</div>
+								))}
+							</SwiperSlide>
+						))}
+					</SliderWithControls>
+				}
+            </div>
         </div>
     )
 }
 
 SlotPage.withHeader = true;
+SlotPage.withFooter = true;
