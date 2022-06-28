595bfd96eb93cbbd734bdc76aeff35d70f175c7e
MaX2708 <grin.maxim.2708@yandex.ru>
2022-06-28 11:08:04 +0400

    password field fix

--- components/PasswordField.js
+++ components/PasswordField.js
@@ -1,10 +1,29 @@
 import { useState } from 'react'
 import styles from '../styles/components/PasswordField.module.css'
+import Image from 'next/image'
 
 export default function PasswordField({...props}) {
-    const [typeF, setTypeF] = useState("password")
+    const [typeF, setTypeF] = useState(true)
 
     return (
-        <input className={styles.input} {...props} type={typeF} />
+        <div className={styles.container}>
+            <input 
+                className={styles.input} 
+                {...props} 
+                type={typeF ? "password" : "text"} 
+            />
+            <div 
+                className={styles.eye}
+                onClick={()=>{
+                    setTypeF(!typeF)
+                }}
+            >
+                <Image
+                    src={typeF ? "/images/icons/eye-off.svg" : "/images/icons/eye.svg"}
+                    width={24}
+                    height={24}
+                />
+            </div>            
+        </div>
     )
 }
